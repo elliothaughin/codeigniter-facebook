3941f03e715d8562900f0db67196d91d9e9ac434
Sean Gates <oregonwebdev@gmail.com>
2011-04-08 20:00:46 -0700

    Added the canvas URL;

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -4,3 +4,4 @@
 	$config['facebook_api_secret'] 		= '';
 	$config['facebook_default_scope']	= ''; // E.G 'read_stream,birthday,users_events,rsvp_event'
 	$config['facebook_api_url'] 		= 'https://graph.facebook.com/'; // Just in case it changes.
+	$config['facebook_canvas_url'] 	= 'https://apps.facebook.com/YOUR_APP_NAME'; // Just in case it changes.
