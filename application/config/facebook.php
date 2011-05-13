88512f7186f7008b07b61284cedee64d9ac0e6c1
mastacheata <mastacheata@gulli.com>
2011-05-13 20:54:11 +0200

    Complete rewrite of the codeIgniter library. Native support for background storage of access tokens. Exception handling is still to be completed and an option for multicurl is planned

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,6 +1,10 @@
-<?php
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 	$config['facebook_app_id'] 			= '';
-	$config['facebook_api_secret'] 		= '';
+	$config['facebook_app_key']         = '';
+	$config['facebook_app_secret'] 		= '';
 	$config['facebook_default_scope']	= ''; // E.G 'read_stream,birthday,users_events,rsvp_event'
 	$config['facebook_api_url'] 		= 'https://graph.facebook.com/'; // Just in case it changes.
+
+/* End of file facebook.php */
+/* Location: ./application/config/facebook.php */
